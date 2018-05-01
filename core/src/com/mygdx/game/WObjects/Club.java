98cac84e1c1202c46e5412fcd0a14f2dba786ede
beppemarinelli <marinellibeppe@gmail.com>
2018-05-01 22:16:46 +0200

    Improved Club class

--- core/src/com/mygdx/game/WObjects/Club.java
+++ core/src/com/mygdx/game/WObjects/Club.java
@@ -13,7 +13,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.mygdx.game.Utils.Helper;
 
-public class Club implements InputProcessor {
+public class Club {
 
     private ModelInstance club;
     private Model model;
@@ -55,45 +55,5 @@ public class Club implements InputProcessor {
 
     public void dispose(){
         model.dispose();
-    }
-
-    @Override
-    public boolean keyDown(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(int amount) {
-        return false;
     }
 }
