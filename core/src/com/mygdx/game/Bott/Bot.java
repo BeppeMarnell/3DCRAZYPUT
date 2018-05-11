47ac7471cd7cf117a82d2364c69cf4375ba4d942
Aleksander Sopiqoti <32581815+AlexSopiqoti@users.noreply.github.com>
2018-05-11 17:37:33 +0200

    Update Bot.java

--- core/src/com/mygdx/game/Bott/Bot.java
+++ core/src/com/mygdx/game/Bott/Bot.java
@@ -34,12 +34,13 @@ public class Bot {
         botMap.reset();
     }
 
+
     private void astar(BotMap botMap){
         Node start = new Node(botMap.start.getX(),botMap.start.getY() );
 
         Node end = new Node(botMap.end.getX(), botMap.end.getY());
 
-        int[][] blockarray = botMap.maze;
+
 
         AStar aStar = new AStar(botMap.maze.length, botMap.maze[0].length, start, end);
 
@@ -51,11 +52,31 @@ public class Bot {
            }
        }
 
+        for(int i =7; i < botMap.maze.length-7; i++){
+            for(int j = 7; j < botMap.maze[0].length-7; j++){
+                if(botMap.maze[i][j]==1){
+                    aStar.setBlock(i+2, j);
+                    aStar.setBlock(i-2, j);
+                    aStar.setBlock(i, j+2);
+                    aStar.setBlock(i ,j-2);
+                    aStar.setBlock(i+2, j+2);
+                    aStar.setBlock(i-2, j+2);
+                    aStar.setBlock(i-2, j-2);
+                    aStar.setBlock(i+2 ,j-2);
+
+                }
+            }
+        }
 
 
         List<Node> path = aStar.findPath();
 
+        System.out.println(path.size());
+        System.out.println(separateShotAStar(path).size());
+
+
         botMap.printPathNode(path);
+
         botMap.reset();
     }
 
@@ -107,4 +128,54 @@ public class Bot {
     public  double calculateX(Coordinate c1, Coordinate c2) {
         return (c2.x - c1.x);
     }
+
+
+
+    public  List<Node> separateShotAStar(List<Node> path) throws IndexOutOfBoundsException {
+        try {
+            List<Node> shotsLV = new ArrayList<>();
+            Node origin = path.get(0);
+            double slope;
+
+            if (path.size() > 1) {
+                slope = calculateSlopeAStar(origin, path.get(1));
+
+                for (int x = 2; x < path.size(); x++) {
+                    double tempSlope = calculateSlopeAStar(origin, path.get(x));
+
+                    if (tempSlope != slope) {
+
+                        origin = path.get(x - 1);
+                        shotsLV.add(origin);
+                        slope = calculateSlopeAStar(origin, path.get(x));
+                    }
+                }
+            }
+
+            return shotsLV;
+        }
+        catch (Exception e ){
+            System.out.println("No Solution!");
+            List<Node> emptyList = new ArrayList<>();
+            return emptyList;
+        }
+
+
+    }
+
+    public  double calculateSlopeAStar(Node c1, Node c2) {
+        if (calculateXAStar(c1, c2) != 0)
+            return calculateYAStar(c1, c2)/(calculateXAStar(c1, c2));
+        else
+            return -1;
+    }
+
+    public  double calculateYAStar(Node c1, Node c2) {
+        return (c2.getCol() - c1.getCol());
+    }
+
+    public  double calculateXAStar(Node c1, Node c2) {
+        return (c2.getRow() - c1.getRow());
+    }
 }
+
