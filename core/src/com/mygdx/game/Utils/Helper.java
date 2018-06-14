7307fcd753469de5edf8e531e424ed1e1c5f0569
BeppeMarnell <35118227+BeppeMarnell@users.noreply.github.com>
2018-06-14 19:38:20 +0200

    Update Helper.java

--- core/src/com/mygdx/game/Utils/Helper.java
+++ core/src/com/mygdx/game/Utils/Helper.java
@@ -49,12 +49,22 @@ public class Helper {
         return ratio * (valueCoord1 - startCoord1) + offset;
     }
 
+    /**
+     * Redundand Method. USE --> MathUtils.clamp (provided by LIBGDX)
+     */
     public static float clamp(float min, float max, float closest) {
         if (closest < min) { return min; }
         if (closest > max) { return max; }
         return closest;
     }
 
+
+    /**
+     * Method to calculate the angle between two points in a 2D environment 
+     * @param vec1 1st vector
+     * @param vec2 2st vector
+     * @return angle in degree, always positive
+     */
     public static float angleBetweenPoints(Vector2 vec1, Vector2 vec2){
         Vector2 one;
         Vector2 two;
@@ -68,5 +78,20 @@ public class Helper {
         }
 
         return  Math.abs(MathUtils.radDeg * MathUtils.atan2((one.y-two.y),(one.x-two.x)));
+    }
+
+    /**
+     * Method to calculate the angle between two points in a 3D environment 
+     * @param vec1 1st vector
+     * @param vec2 2st vector
+     * @return angle in degree, always positive
+     */
+    public static float angleBetweenPoints3D(Vector3 vec1, Vector3 vec2){
+        // create a normalized vector and take just the height of the vec1
+        Vector2 from = new Vector2(0, vec1.y);
+        
+        //translate the distance between the two 3D point as distance in a 2D normalized context
+        Vector2 to = new Vector2(vec1.dst(vec2), vec2.y); // 
+        return  angleBetweenPoints(from, to);
     }
 }
